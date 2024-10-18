11bc22c62e68cbe162291cf6d4779cd8a03cdc2a
Opeyemi Esan <tolulopejossyp@gmail.com>
2024-10-18 11:49:02 +0100

    first commit

---
+++ Controllers/GreetingController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BincomHelloWorld.Controllers
+{
+    public class GreetingController : Controller
+    {
+        [HttpGet("greeting")]
+        public IActionResult Greeting()
+        {
+            return Content("Hello World");
+        }
+    }
+}
